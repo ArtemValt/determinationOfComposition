5e067d50290276a5d6733218de88f568156e1f40
Artem <artemvalt12@gmail.com>
2023-01-19 21:17:42 +0400

    init commit

---
+++ src/main/java/com/example/determination_of_composition/DeterminationOfCompositionApplication.java
@@ -0,0 +1,13 @@
+package com.example.determination_of_composition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeterminationOfCompositionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeterminationOfCompositionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/determination_of_composition/DeterminationOfCompositionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.determination_of_composition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeterminationOfCompositionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
